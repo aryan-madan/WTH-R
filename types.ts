42fbb0d500afdcd00a90e1bd5ebf51aea17b00ba
Aryan Madan <madan.aryy@gmail.com>
2025-11-26 14:29:50 +0530

    fixed types

--- types.ts
+++ types.ts
@@ -1,42 +1,50 @@
-export interface Weather {
-  current: {
-    temp: number;
-    code: number;
-    day: number;
-    wind: number;
-  };
-}
+  export interface Weather {
+    current: {
+      temp: number;
+      code: number;
+      day: number;
+      wind: number;
+      humidity: number;
+      feels_like: number;
+      pressure: number;
+      precip: number;
+      wind_dir: number;
+      cloud: number;
+      pm25: number;
+      aqi: number;
+    };
+  }
 
-export interface Place {
-  lat: number;
-  lon: number;
-  name: string;
-  admin?: string;
-  country?: string;
-}
+  export interface Place {
+    lat: number;
+    lon: number;
+    name: string;
+    admin?: string;
+    country?: string;
+  }
 
-export interface Msg {
-  head: string;
-  sub: string;
-}
+  export interface Msg {
+    head: string;
+    sub: string; 
+  }
 
-export interface Content {
-  main: string;
-  outline: string;
-  sub: string;
-}
+  export interface Content {
+    main: string;
+    outline: string;
+    sub: string;
+  }
 
-export interface Report {
-  current: {
-    temp: number;
-    code: number;
-    day: number;
-    wind: number;
-  };
-}
+  export interface Report {
+    current: {
+      temp: number;
+      code: number;
+      day: number;
+      wind: number;
+    };
+  }
 
-export interface Coords {
-  lat: number;
-  lon: number;
-  city?: string;
-}
+  export interface Coords {
+    lat: number;
+    lon: number;
+    city?: string;
+  }
